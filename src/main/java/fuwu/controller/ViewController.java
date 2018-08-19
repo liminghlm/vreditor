3a0e1b3a2b4a63792f611eab562191d036a106bd
ypc1996 <624474697@qq.com>
2018-08-19 10:33:41 +0800

    transci delete

--- src/main/java/fuwu/controller/ViewController.java
+++ src/main/java/fuwu/controller/ViewController.java
@@ -20,7 +20,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -94,8 +93,7 @@ public class ViewController {
 
     }
     @ResponseBody
-    @RequestMapping(value = "/setMainView", method = RequestMethod.GET)
-    @Transactional
+    @RequestMapping(value = "/setMainView", method = RequestMethod.POST)
     @ForLog
     public JsonResult setMainView(Integer viewId){
         View view= viewMapper.getViewByViewId(viewId);
